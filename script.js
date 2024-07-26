f6230f5ba0e02a23411e2050b408d429407230e3
henriqueluizcpradi <137069786+henriqueluizcpradi@users.noreply.github.com>
2024-07-26 08:01:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+//para facilitar na hr de puxar no console.log
+const aluno = "ALUNO: "
+const nota = "NOTA:"
+
+//definindo os nomes
+const alunos =['ADRIANO NOVISKI GOMES DOS SANTOS',
+'EMILLY CRISANTO DA CRUZ FREIRE',
+'EMILLY PRISCILA MACEDO VIEIRA.',
+'FELIPE DE SOUZA MOTTA',
+'GUILHERME DE VITO GEBING',
+'HEITOR FERREIRA DOS SANTOS',
+'IZABELLA MALUCELLI',
+'KAUA DHYEGO PRADO FRANÇA',
+'KAUAN JOSÉ VIOLA MARCELINO',
+'LAUANA DA ROSA QUADROS',
+'LIVIA BICALHO DOS SANTOS',
+'LUAN CARLOS MOREIRA',
+'MARIA HELENA DOS SANTOS MUCCI DANIEL',
+'NICOLE GABRIELE MIRANDA LOBATO',
+'RAQUEL ESTER CRUZ PACHECO',
+'RENATO SOUZA ALVES JUNIOR',
+'KAUA MOURA DE SOUZA',
+'LUCAS DA SILVA SALVADOR',]
+
+//definindo notas
+const notas = [10,0]
+
+//espaçamento
+console.log('-----------------------------------------')
+
+//cabeçalho
+console.log('Lista de alunos e seus numeros')
+
+//espaçamento
+console.log('-----------------------------------------')
+
+//puxando os dados
+console.log(' 1 - ' + aluno + alunos[0]  )
+console.log(' 2 - ' + aluno + alunos[1]  )
+console.log(' 3 - ' + aluno + alunos[2]  )
+console.log(' 4 - ' + aluno + alunos[3] )
+console.log(' 5 - ' + aluno + alunos[4] )
+console.log(' 6 - ' + aluno + 'HENRIQUE LUIZ CAMILO PRADI' )
+console.log(' 7 - ' + aluno + alunos[6] )
+console.log(' 8 - ' + aluno + alunos[7] )
+console.log('10 - ' + aluno + alunos[8] )
+console.log('11 - ' + aluno + alunos[9] )
+console.log('13 - ' + aluno + alunos[10] )
+console.log('14 - ' + aluno + alunos[11] )
+console.log('16 - ' + aluno + alunos[12] )
+console.log('17 - ' + aluno + alunos[13] )
+console.log('18 - ' + aluno + alunos[14] )
+console.log('19 - ' + aluno + alunos[15] )
+console.log('21 - ' + aluno + alunos[16] )
+console.log('22 - ' + aluno + alunos[17] )
+
+//espaçamento
+console.log('-----------------------------------------')
